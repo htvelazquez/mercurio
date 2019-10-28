74156c816ae7a45f862a4d464acc3bfba124ea02
htvelazquez <htvelazquez@gmail.com>
2019-10-27 23:59:20 -0300

    Cirenio Banking backend

---
+++ cirenio_banking/backend/views/header.php
@@ -0,0 +1,9 @@
+<head>
+    <title>Banking Solution :: Cirenio</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="/views/css/bootstrap.css">
+    <link rel="stylesheet" href="/views/css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+</head>
---
+++ cirenio_banking/backend/views/home.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php Flight::render("header"); ?>
+
+<body>
+  <nav class="navbar navbar-default top-navbar" role="navigation">
+    <div class="navbar-header">
+        <a class="navbar-brand" href="/"><span class="brand" >Cirenio</span> Banking</a>
+    </div>
+  </nav>
+
+  <div class="container" style="margin-top:80px;">
+    <div class="row justify-content-center mb-3 pb-3">
+        <div class="col-md-7 text-center heading-section ftco-animate fadeInUp ftco-animated">
+            <h2 class="mb-4" style="text-align:left">Para comenzar escoge tu banco:</h2>
+        </div>
+    </div>
+    <div class="row" style="margin-top:100px">
+      <div class="table-responsive">
+        <table id="banks" class="table">
+          <tbody>
+              <tr>
+            <?php $i=1; foreach($banks as $bank) { ?>
+              <td class="<?php echo $bank['alias'] ?> background-bw bank-background" >
+                <a href="/form/<?php echo $bank['alias'].'/'.$bank['id'] ?>" title="<?php echo $bank['name'] ?>" ></a>
+              </td>
+              <?php if ($i % 4 == 0) { ?> </tr><tr> <?php } ?>
+            <? $i++; } ?>
+              </tr>
+          </tbody>
+        </table>
+      </div>
+
+    </div>
+</div>
+</body>
+
+</html>
